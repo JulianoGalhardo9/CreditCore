928b8e549925a7c2e1c7253b5c55fff0ea7b9bbf
Juliano Galhardo <jubagalhardo9@gmail.com>
2026-04-06 17:00:51 -0300

    feat(gateway): aggregate microservices swagger documentation into a single gateway UI

--- ApiGateway/Gateway.Api/Program.cs
+++ ApiGateway/Gateway.Api/Program.cs
@@ -3,7 +3,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+
+    options.SwaggerEndpoint("/api/User/swagger/v1/swagger.json", "Identity Service");
+    options.SwaggerEndpoint("/api/Loan/swagger/v1/swagger.json", "Credit Service");
+    options.RoutePrefix = string.Empty;
+});
 
 app.MapReverseProxy();
 
